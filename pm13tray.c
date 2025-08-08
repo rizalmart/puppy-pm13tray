e9f2aa3ac1c5d0acd01dc807f08e039efc951269
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2025-08-08 11:10:01 +0800

    Update pm13tray.c

--- pm13tray.c
+++ pm13tray.c
@@ -7,7 +7,7 @@
 AppIndicator *tray_icon;
 GtkWidget *menu;
 
-/* XPM */
+/* XPM icon */
 static char *floppy_xpm[] = {
 "28 28 62 1",
 " 	c None",
@@ -102,11 +102,7 @@ static char *floppy_xpm[] = {
 "                            "
 };
 
-void tray_icon_on_click(GtkMenuItem *item, gpointer user_data) {
-    system("eventmanager pm13 &");
-}
-
-/* Write XPM data to a temporary file and return the filename */
+/* Write XPM icon to temp file */
 char* write_temp_xpm_icon() {
     static char filename[] = "/tmp/iconXXXXXX.xpm";
     int fd = mkstemps(filename, 4); // ".xpm"
@@ -130,6 +126,17 @@ char* write_temp_xpm_icon() {
     return filename;
 }
 
+/* Handle menu item actions */
+void tray_icon_on_click(GtkMenuItem *item, gpointer user_data) {
+    const char *action = (const char *)user_data;
+
+    if (g_strcmp0(action, "save") == 0) {
+        system("eventmanager save-session &");
+    } else if (g_strcmp0(action, "settings") == 0) {
+        system("eventmanager pm13 &");
+    }
+}
+
 /* Create AppIndicator tray icon */
 static AppIndicator *create_tray_icon() {
     char *icon_path = write_temp_xpm_icon();
@@ -140,9 +147,16 @@ static AppIndicator *create_tray_icon() {
 
     menu = gtk_menu_new();
 
-    GtkWidget *menu_item = gtk_menu_item_new_with_label("Open Pupmode 13 Settings");
-    g_signal_connect(menu_item, "activate", G_CALLBACK(tray_icon_on_click), NULL);
-    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
+    // Menu item: Save Session Now
+    GtkWidget *save_item = gtk_menu_item_new_with_label("Save Session Now");
+    g_signal_connect(save_item, "activate", G_CALLBACK(tray_icon_on_click), (gpointer)"save");
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), save_item);
+
+    // Menu item: Open Pupmode 13 Settings
+    GtkWidget *settings_item = gtk_menu_item_new_with_label("Open Pupmode 13 Settings");
+    g_signal_connect(settings_item, "activate", G_CALLBACK(tray_icon_on_click), (gpointer)"settings");
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), settings_item);
+
     gtk_widget_show_all(menu);
 
     tray_icon = app_indicator_new(
@@ -160,9 +174,7 @@ static AppIndicator *create_tray_icon() {
 
 int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
-
     create_tray_icon();
-
     gtk_main();
     return 0;
 }
